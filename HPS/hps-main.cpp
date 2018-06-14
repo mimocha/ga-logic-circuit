6626a067ade4c887389768dc4da7ee9465a6d0dc
mimocha.zen <l.chawit@outlook.com>
2018-06-14 23:14:59 +0900

    Changed hps-main.cpp function: evaluate() to getoutput() For later expansion, getting output from several FPGA circuits. Possible problem with timing, should add delay between setting circuit and checking results.

--- HPS/hps-main.cpp
+++ HPS/hps-main.cpp
@@ -5,13 +5,23 @@
 #include <string.h>
 #include <iostream>
 #include <bitset>
+#include <time.h>
 
-int evaluate (int input, char tt);
+time_t test_time;
+
+char getoutput (unsigned short input, unsigned short state);
+void delay (int cycles);
 
 int main (int argc, char* argv[]) {
-	static int flag_i[2], flag_o[2], flag_d[2], flag_tt[2]; // option flags
-	static int input, output; // input-count, output-count, truth-table
-	static char tt;
+
+// ----- ----- Setup ----- ----- //
+
+	unsigned short flag_i[2], flag_o[2], flag_d[2], flag_tt[2]; // option flags
+	unsigned short input, output; // input-count, output-count
+	unsigned char tt; // Truth Table in hexadecimal
+
+	time(&test_time);
+	printf("%d\n",test_time);
 
 	/* Option Arguments:
 		flag_x[0] == Option "-x" was used
@@ -50,12 +60,11 @@ int main (int argc, char* argv[]) {
 	// Option -i: Input
 	if (flag_i[0]) { // Load Value
 		input = strtol(argv[flag_i[1]+1], NULL, 10);
+	} else {
+		input = 0;
 	}
 	if (flag_d[0]) { // Debug Info
-		if (flag_i[0] == 0) {
-			printf("Input argument -i not found!\n");
-			// return -1;
-		} else if (input <= 0) {
+		if (input <= 0) {
 			printf("Invalid argument -i: %d\n", input);
 			// return -1;
 		} else {
@@ -66,12 +75,11 @@ int main (int argc, char* argv[]) {
 	// Option -o: Output
 	if (flag_o[0]) { // Load Value
 		output = strtol(argv[flag_o[1]+1], NULL, 10);
+	} else {
+		output = 0;
 	}
 	if (flag_d[0]) { // Debug Info
-		if (flag_o[0] == 0) {
-			printf("Input argument -o not found!\n");
-			// return -1;
-		} else if (output <= 0) {
+		if (output <= 0) {
 			printf("Invalid argument -o: %d\n", output);
 			// return -1;
 		} else {
@@ -93,19 +101,77 @@ int main (int argc, char* argv[]) {
 			printf("Input argument -tt not found. Default to: 0000.\n");
 		} else {
 			printf("Argument -tt: ");
-			std::bitset<4> foo (tt);
-			std::cout << foo << std::endl;
+			std::bitset<4> bit_in (tt);
+			std::cout << bit_in << std::endl;
 		}
 	}
 
+// ----- ----- Evaluation ----- ----- //
+
 	// Put Evaluation Code Here
-	evaluate(input, tt);
+	unsigned short correct, state = 1;
+	unsigned char result;
+	// float accuracy = 0;
+
+	// Calculate the number of truth table state (2^input)
+	for (short i=0; i<input; i++)
+		state *= 2;
+
+	// Runs function getoutput, assign result to char result.
+	// To be expanded later -> get output of multiple FPGA circuits
+	result = getoutput(input, state);
+
+	correct = ~(result^tt); // count the number of ones
+
+	// accuracy = (float)correct/state;
+
+	if (flag_d[0]) {
+		puts("\n Evaluation Results:");
+		std::bitset<4> bit_in (tt);
+		std::cout << "Expected Output: " << bit_in << std::endl;
+		std::bitset<4> bit_out (result);
+		std::cout << "Actual Output: " << bit_out << std::endl;
+		std::bitset<4> bit_cor (correct);
+		std::cout << "Input XNOR Output: " << bit_cor << std::endl;
+	}
 
 	// Return results to PC as fitness score
 
 	return 0;
 }
 
-int evaluate (int input, char tt) {
-	// Evaluates 2^input state
+char getoutput (unsigned short input, unsigned short state) {
+	/* Changes input to FPGA circuit, and records output, bit-by-bit
+	-> Concerns about clock speed, and circuit timing?
+	-> wait function?
+	Returns output in hexadecimal format
+
+	Input - Output Format:
+	Input [MSB : LSB] | Output [MSB : LSB]
+		[1]	[0] | Output [N]
+		--------------------
+		0	0	| Output [3]
+		0	1	| Output [2]
+		1	0	| Output [1]
+		1	1	| Output [0]
+	*/
+	unsigned short i;
+	char result = 0;
+
+	for (i=0; i<state; i++) {
+		result >>= 1; // Shift 1 bit to the right
+		// Set Inputs to state i
+		// MSB format
+		if ( i%2 == 0 ) {
+			result |= 8; // result = result | 1000
+		}
+	}
+
+	return result;
+}
+
+void delay (int cycles) {
+	for (int i=cycles; i!=0; i--) {
+		// No Operation
+	}
 }
