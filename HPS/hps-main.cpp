40bb744995052ad7422837bcd7de933aa5736b7f
mimocha.zen <l.chawit@outlook.com>
2018-06-15 22:17:39 +0900

    Working truth table comparison program. Removed <bitset>.

--- HPS/hps-main.cpp
+++ HPS/hps-main.cpp
@@ -4,18 +4,20 @@
 #include <stdio.h>
 #include <string.h>
 #include <iostream>
-#include <bitset>
 
-char getoutput (unsigned short input, unsigned short state);
-// void delay (int cycles);
+char getoutput (unsigned short state);
 
 int main (int argc, char* argv[]) {
 
 // ----- ----- Setup ----- ----- //
 
-	unsigned short flag_i[2], flag_o[2], flag_d[2], flag_tt[2]; // option flags
-	unsigned short input, output; // input-count, output-count
-	unsigned char tt; // Truth Table in hexadecimal
+	// Option Flag & input/output-count
+	static unsigned short flag_i[2], flag_o[2],
+						flag_d[2], flag_tt[2],
+						input, output;
+	// Truth Table in hexadecimal
+	// Expect = Correct Answer | Actual = FPGA Output Answer | XNOR = Check answer
+	static unsigned char tt_expect, tt_actual, tt_xnor;
 
 	/* Option Arguments:
 		flag_x[0] == Option "-x" was used
@@ -55,12 +57,11 @@ int main (int argc, char* argv[]) {
 	if (flag_i[0]) { // Load Value
 		input = strtol(argv[flag_i[1]+1], NULL, 10);
 	} else {
-		input = 0;
+		input = 2;
 	}
 	if (flag_d[0] == 1) { // Debug Info
 		if (input <= 0) {
-			printf("Invalid argument -i: %d\n", input);
-			// return -1;
+			printf("Invalid argument -i: %d. Default to 2.\n", input);
 		} else {
 			printf("Argument -i: %d\n", input);
 		}
@@ -70,12 +71,11 @@ int main (int argc, char* argv[]) {
 	if (flag_o[0]) { // Load Value
 		output = strtol(argv[flag_o[1]+1], NULL, 10);
 	} else {
-		output = 0;
+		output = 1;
 	}
 	if (flag_d[0] == 1) { // Debug Info
 		if (output <= 0) {
-			printf("Invalid argument -o: %d\n", output);
-			// return -1;
+			printf("Invalid argument -o: %d. Default to: 1\n", output);
 		} else {
 			printf("Argument -o: %d\n", output);
 		}
@@ -86,62 +86,51 @@ int main (int argc, char* argv[]) {
 		// Load truth table as hexadecimal 0-F
 		// Quick & dirty code for now,
 		// Handles 2-bit input, 1-bit output = 4 x 1-bit output
-		tt = strtol(argv[flag_tt[1]+1], NULL, 16);
+		tt_expect = strtol(argv[flag_tt[1]+1], NULL, 16);
 	} else {
-		tt = 0;
+		tt_expect = 0;
 	}
 	if (flag_d[0] == 1) { // Debug Info
 		if (flag_tt[0] == 0) {
 			printf("Input argument -tt not found. Default to: 0000.\n");
 		} else {
-			printf("Argument -tt: ");
-			std::bitset<4> bit_in (tt);
-			std::cout << bit_in << std::endl;
+			printf("Argument -tt: %X\n", tt_expect);
 		}
 	}
 
 // ----- ----- Evaluation ----- ----- //
-
-	// Put Evaluation Code Here
-	unsigned short correct, state = 1;
-	unsigned char result;
+	unsigned short correct = 0, state = 4;
 	float accuracy;
 
-	// Calculate the number of truth table state (2^input)
-	for (short i=0; i<input; i++)
-		state *= 2;
-
-	// Runs function getoutput, assign result to char result.
-	// To be expanded later -> get output of multiple FPGA circuits
-	result = getoutput(input, state);
-
-	correct = ~(result^tt); // count the number of ones
-	std::bitset<4> bit_acc (correct);
-	accuracy = ((float)bit_acc.count() / bit_acc.size());
-
-	if (flag_d[0] == 1) {
-		puts("\n Evaluation Results:");
-		std::bitset<4> bit_in (tt);
-		std::cout << "Expected Output: " << bit_in << std::endl;
-		std::bitset<4> bit_out (result);
-		std::cout << "Actual Output: " << bit_out << std::endl;
-		std::bitset<4> bit_cor (correct);
-		std::cout << "Input XNOR Output: " << bit_cor << std::endl;
-		std::cout << "Number of ones: " << bit_cor.count() << std::endl;
-		std::cout << "Number of zeroes: " << bit_cor.size() - bit_cor.count() << std::endl;
-		printf("Accuracy = %.2f\n", accuracy);
+	// If more than 2 inputs -> set state count
+	if (input != 2) {
+		state = 1;
+		for (int i=0; i<input; i++) {
+			state *= 2;
+		}
 	}
 
-	// Return results to PC as fitness score
+	tt_actual = getoutput(state);
 
-	return bit_acc.count();
+	// XNOR function sets bit which are the same to 1 (aka. A == B)
+	tt_xnor = ~(tt_expect ^ tt_actual);
+
+	if ((tt_xnor & 8) != 0) correct++;
+	if ((tt_xnor & 4) != 0) correct++;
+	if ((tt_xnor & 2) != 0) correct++;
+	if ((tt_xnor & 1) != 0) correct++;
+
+	accuracy = (float) correct / state;
+	printf("Correct: %d | Accuracy: %.2f\n", correct, accuracy);
+
+	return correct;
 }
 
-char getoutput (unsigned short input, unsigned short state) {
+char getoutput (unsigned short state) {
 	/* Changes input to FPGA circuit, and records output, bit-by-bit
 	-> Concerns about clock speed, and circuit timing?
 	-> wait function?
-	Returns output in hexadecimal format
+	Returns output as char, 4-bit result
 
 	Input - Output Format:
 	Input [MSB : LSB] | Output [MSB : LSB]
@@ -151,24 +140,17 @@ char getoutput (unsigned short input, unsigned short state) {
 		0	1	| Output [2]
 		1	0	| Output [1]
 		1	1	| Output [0]
+
+	Return in MSB format
 	*/
-	unsigned short i;
+	volatile unsigned short n, i = 1;
 	char result = 0;
 
-	for (i=0; i<state; i++) {
-		result >>= 1; // Shift 1 bit to the right
-		// Set Inputs to state i
-		// MSB format
-		if ( i%2 == 0 ) {
-			result |= 8; // result = result | 1000
-		}
+	for (n=1; n<=state; n++) {
+		if ( 1 ) // Evaluation Condition
+			result |= i; // Set result bit "n" to 1
+		i *= 2;
 	}
 
 	return result;
 }
-
-// void delay (int cycles) {
-// 	for (int i=cycles; i!=0; i--) {
-// 		// No Operation
-// 	}
-// }
